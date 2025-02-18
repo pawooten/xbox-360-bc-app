546fb1e3b950335e6c5be50a384cfdfef3aa82a2
Paul Wooten <paul.wooten@hyland.com>
2025-02-18 14:22:50 -0500

    feat: express plaintext response

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log('XBOX 360 BC App');
+import express from 'express';
+
+const title = 'XBOX 360 BC App';
+console.log(title);
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.status(200).send(title);
+  });
+  
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
