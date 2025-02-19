e902f9cd5f497c85e69da38d377e69d86f1ad564
Paul Wooten <paul.wooten@hyland.com>
2025-02-19 09:35:31 -0500

    refactor: extract string literal constants

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-import { GameData, load, loadSettings, Settings } from './json-loader';
+import { load, loadSettings } from './json-loader';
 import { searchGames } from './search';
+import { Directories as DirectoryNames, LogMessages, QueryParameters, RoutePaths } from './constants';
 
 const gameData = load();
 if (gameData.error) {
@@ -15,14 +16,14 @@ if (settings.title) {
 console.log(gameData.message);
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(DirectoryNames.Public));
 
-app.get('/search', (req, res) => {
-    const result = searchGames(req.query.q as string, settings, gameData);
+app.get(RoutePaths.Search, (req, res) => {
+    const result = searchGames(req.query[QueryParameters.Query] as string, settings, gameData);
     res.status(200).send(result);
     return;
   });
   
 app.listen(settings.port, () => {
-  console.log(`Server is running on port ${settings.port}`);
+  console.log(LogMessages.ServerIsRunning(settings.port));
 });
---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+export const Directories = {
+    Public: 'public',
+};
+
+export const LogMessages = {
+    ServerIsRunning: (port: number) => `Server is running on port ${port}`,
+};
+
+export const RoutePaths = {
+    Search: '/search',
+};
+
+export const QueryParameters = {
+    Query: 'q',
+};
